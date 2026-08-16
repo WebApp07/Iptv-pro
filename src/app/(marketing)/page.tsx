50bf3e4d7a961c6e98bee90110d6b2124d25af05
WebApp07 <aminezouguig@proton.me>
2026-08-17 01:34:05 +0200

    chore: remove unused home sections

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,8 +1,6 @@
 import { Hero } from "@/components/home/hero";
 import { Streaming } from "@/components/home/streaming";
 import { Sports } from "@/components/home/sports";
-import { Channels } from "@/components/home/channels";
-import { Features } from "@/components/home/features";
 import { HowItWorks } from "@/components/home/how-it-works";
 import { PricingTeaser } from "@/components/home/pricing-teaser";
 import { Testimonials } from "@/components/home/testimonials";
@@ -15,8 +13,6 @@ export default function HomePage() {
       <Hero />
       <Streaming />
       <Sports />
-      <Channels />
-      <Features />
       <HowItWorks />
       <PricingTeaser />
       <Testimonials />
